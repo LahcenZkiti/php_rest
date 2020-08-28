3a931b36be9c4de2568ad297a6b083177f1f573f
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-29 00:33:41 +0100

    read

---
+++ api/post/read.php
@@ -0,0 +1,34 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate blog post object
+$post = new Post($db);
+
+// Blog post query
+$result = $post->read();
+
+// Get row count
+$num = $result->rowCount();
+
+// Ckeck if any posts
+if($num > 0) {
+    // Post array
+    $posts_arr = array();
+    $psots_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        
+    }
+    
+}
