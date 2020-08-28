ab50ad099c8f2e52656270d8ed1efad10ba023e4
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-28 17:58:23 +0100

    added post model

---
+++ models/Post.php
@@ -0,0 +1,44 @@
+<?php
+class Post{
+    // DB stuff
+    private $conn;
+    private $table = 'posts';
+
+    // Post properties
+    public $id;
+    public $sategory_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+    
+    // Constructor with DB
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // Get Posts
+    public function read(){
+        // Create query
+        $query = 'SELECT c.name as category_name,
+                    p.id, 
+                    p.ctegory_id, 
+                    p.title, 
+                    p.body, 
+                    p.created_at
+                FROM ' . $this->table . 'p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                ORDER BY
+                    p.created_at DESC';
+        
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
