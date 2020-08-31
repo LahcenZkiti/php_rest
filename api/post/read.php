c18b5b0a688bc52e4eaa9047d8d783f8cba1a583
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-31 23:22:51 +0100

    post array

--- api/post/read.php
+++ api/post/read.php
@@ -28,7 +28,23 @@ if($num > 0) {
     while($row = $result->fetch(PDO::FETCH_ASSOC)){
         extract($row);
 
+        $post_item =  array(
+            'id' => $id,
+            'title' => $title,
+            'body' => html_entity_decode($body),
+            'author' => $author,
+            'category_id' => $category_id,
+            'category_name' => $category_name
+        );
+
+        // Push to 'data'
+        array_push($posts_arr['data'], $post_item);
         
     }
     
+} else {
+    // No posts
+    echo json_decode(
+        array('message' => 'No Posts Found')
+    );
 }
