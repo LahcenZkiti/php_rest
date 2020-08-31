9ea5a98f7f532fd5b8ebac72cdfbf9f569c87a14
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-31 23:23:14 +0100

    Fix typo

--- config/Database.php
+++ config/Database.php
@@ -2,7 +2,7 @@
 class Database {
     // DB params
     private $host = 'localhost';
-    private $db_name = 'myfirstrest';
+    private $db_name = 'myblog';
     private $username = 'root';
     private $password = '1234';
     private $conn;
