ff09f063a56f94ebe430e8343ee11e55cf9632f2
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-01 16:14:18 +0100

    read_single

--- models/Post.php
+++ models/Post.php
@@ -42,4 +42,41 @@ class Post{
 
         return $stmt;
     }
+
+    // Get single Post
+
+    public function read_single(){
+        // Create query
+        $query = 'SELECT c.name as category_name, 
+                    p.id, 
+                    p.category_id, 
+                    p.title, 
+                    p.body,
+                    p.author, 
+                    p.created_at
+                FROM ' . $this->table . ' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                WHERE 
+                    p.id = ?
+                LIMIT 0,1';
+        
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind ID
+        $stmt->bindParam(1,$this->id);
+
+        // Execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Set properties
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+    }
 }
